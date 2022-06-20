b8f187d5581578e10cbdcf45524a13439c161b81
HZloto <zlotowski.hugo@alumni.esade.edu>
2022-06-20 17:11:40 +0200

    added get_song_features function

--- recommender.py
+++ recommender.py
@@ -12,9 +12,8 @@
 import pandas as pd
 import requests
 
-song_id = '2QuSUJTRJMzWssW9nXPGcf' # Id of the spotify target sound
 
-def get_song_features(song_id: str = song_id, client_id: str = "069700c64b6d428088724522a691188c", client_secret: str = "3cb0e989241f47df8cc6f79a34f8ff8a") -> pd.DataFrame:
+def get_song_features(song_id: str, client_id: str = "069700c64b6d428088724522a691188c", client_secret: str = "3cb0e989241f47df8cc6f79a34f8ff8a") -> pd.DataFrame:
     '''
     
     This fuction takes a song spotify ID and returns a pandas dataframe with its features
@@ -56,9 +55,18 @@ def get_song_features(song_id: str = song_id, client_id: str = "069700c64b6d4280
     song_attributes = requests.get(url, headers = headers , params = params).json()
 
     df_song_input = pd.DataFrame(song_attributes["audio_features"])
+    df_song_input = df_song_input[['danceability', 'energy', 'loudness', 'mode', 'speechiness',
+       'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo','id']]
     return df_song_input
 
+def get_target_df(df_target_path: str = 'temp_recommender.csv') -> pd.DataFrame:
 
-df_target = pd.read_csv('temp_recommender.csv')
+    df_target = pd.read_csv(df_target_path)
+    df_target = df_target[['danceability', 'energy', 'loudness', 'mode', 'speechiness',
+        'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo','track_id']]
+    df_target = df_target.rename(columns={'track_id':'id'})
+    return df_target
 
-print(get_song_features())
+
+df_song_input = get_song_features(song_id = '2QuSUJTRJMzWssW9nXPGcf') # Id of the spotify target sound
+df_target = get_target_df(df_target_path = 'temp_recommender.csv')
