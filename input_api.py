15fff210ce5dbc51ed87a45764ba166cb8e097a6
Martingarcia78 <martingarcia78@hotmail.com>
2022-06-21 12:21:34 -0500

    Creation of connection class. Later this class can be used for the scripts.

--- input_api.py
+++ input_api.py
@@ -2,6 +2,28 @@
 # - Spotify API
 # - Output: vector/list/array with all the song’s features
 # MARTIN
+import requests
+
+class connection():
+    def __init__(self) -> None:
+        self.__client_id = "6501d860b4b34349b45b2af4b1d9b5c9"
+        self.__client_secret = "e034edd0be29447e90914e512e359625"
+
+    def get_auth(self):
+        
+
+inp = input('Type your song')
+
+# URL for token resource
+auth_url = 'https://accounts.spotify.com/api/token'
+
+# request body
+params = {'grant_type': 'client_credentials',
+          'client_id': client_id,
+          'client_secret': client_secret}
+
+# POST the request
+auth_response = requests.post(auth_url, params).json()
 
 # 1. Create a function that takes as an input the name of one artist
 # (We only ask for artist name for simplicity)
@@ -12,4 +34,3 @@ test_artist = "Bad Bunny"
 # 3.return artist name as a string and best song as a df with the following features:
 # ['danceability', 'energy', 'loudness', 'mode', 'speechiness',
 #   'acousticness', 'instrumentalness', 'liveness', 'valence', 'tempo']
-
