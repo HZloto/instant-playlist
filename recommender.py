99417bc585a043423c5ab346cbcbd6a0cc19a975
HZloto <zlotowski.hugo@alumni.esade.edu>
2022-06-22 12:11:26 +0200

    added random text

--- recommender.py
+++ recommender.py
@@ -3,6 +3,7 @@
 # - Compare song output with our database csv
 # - Output: List of top 10 songs in terms of similarities (define how we value similarity)
 # HUGO
+#Random text
 
 # 1. give spotify song id
 # 2. give a csv
